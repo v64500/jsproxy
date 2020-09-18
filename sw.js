c6977090009436be2d912a4eba61cd2b2aa6e139
qin <v645@outlook.com>
2020-09-18 13:09:26 +0800

    match gg

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__='assets/bundle.4c121040.js')};importScripts('conf.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__='assets/bundle.e06c70d0.js')};importScripts('conf.js')
