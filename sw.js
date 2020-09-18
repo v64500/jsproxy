69d8ef1a0184f8676435441b4a0eed22afdb5842
qin <v645@outlook.com>
2020-09-18 11:21:10 +0800

    load fail

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__='assets/bundle.34f2b044.js')};importScripts('conf.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__='assets/bundle.ecd8a16c.js')};importScripts('conf.js')
