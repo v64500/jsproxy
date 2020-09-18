96f05ed77d19449a7e87f9e029521a512daa021e
qin <v645@outlook.com>
2020-09-18 17:04:10 +0800

    match gg

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__='assets/bundle.e1095249.js')};importScripts('conf.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__='assets/bundle.d177e957.js')};importScripts('conf.js')
